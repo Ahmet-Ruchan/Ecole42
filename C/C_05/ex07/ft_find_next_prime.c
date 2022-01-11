b17c08f761636f6954f2b815600d61e5e38624cf
Ahmet Ruçhan Avcı <aruchanavci01@gmail.com>
2022-01-11 21:09:13 +0300

    ecole 42 piscine
    Work carried out throughout the piscine

---
+++ C/C_05/ex07/ft_find_next_prime.c
@@ -0,0 +1,22 @@
+int	ft_is_prime(int nb)
+{
+	int	n;
+
+	n = 2;
+	if (nb < 2)
+		return (0);
+	while (n <= nb / 2)
+	{
+		if (nb % n == 0)
+			return (0);
+		n++;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	while (nb < 2147483647 && !ft_is_prime(nb))
+		nb++;
+	return (nb);
+}
